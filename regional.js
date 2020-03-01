535ccb2dc1e9d8407a16b3f574e4b8f009969293
Nicole Reynoldson <nicoleareynoldson@gmail.com>
2020-03-01 14:52:21 -0800

    Updated to allow for variations on USA search

--- regional.js
+++ regional.js
@@ -9,43 +9,45 @@ function bindButtons(){
         var req = new XMLHttpRequest();
         var location = document.getElementById('region').value;
        
-        if(location != ""){
-            var requestURL = 'https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=' + location + '&api_key=' + apiKey + '&format=json';
-                
-            req.open('GET', requestURL, true);
-            req.send(null);
-         
-            req.addEventListener('load', function(){
-                var response = JSON.parse(req.responseText);
-                
-                
-            let listTitle = document.getElementById("regionList");
-            listTitle.textContent = "Showing Top Artists for " + response.topartists["@attr"].country;
-            listTitle.appendChild(document.createElement('hr'));
-                
-                
-                var artistArray = [];
-                for(var i = 0; i < 10; i++){
-                    artistArray.push(response.topartists.artist[i]);
-                    
-                let regionArtist = document.createElement("li");
-                    document.getElementById("regionList").appendChild(regionArtist);
-                    regionArtist.textContent = artistArray[i].name;
-                    
-                    let listeners = document.createElement("ul");
-                   listeners.className = "subList";
-                    regionArtist.appendChild(listeners);
-                    let bullet = document.createElement("li");
-                    bullet.className = "subBullet";
-                    listeners.appendChild(bullet);
-                     bullet.textContent = "Listeners: " + parseInt(artistArray[i].listeners).toLocaleString();
-                }
-                
-                
-                })    
+        if(location.toLowerCase() == 'usa' || location.toLowerCase() == 'us' || location.toLowerCase() == 'america')
+            location = 'united states';
         
-            event.preventDefault();  
+        var requestURL = 'https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=' + location + '&api_key=' + apiKey + '&format=json';
+                
+        req.open('GET', requestURL, true);
+        req.send(null);
+         
+        req.addEventListener('load', function(){
+        var response = JSON.parse(req.responseText);
+                
+                
+        let listTitle = document.getElementById("regionList");
+        listTitle.textContent = "Showing Top Artists for " + response.topartists["@attr"].country;
+        listTitle.appendChild(document.createElement('hr'));
+                
+                
+        var artistArray = [];
+        for(var i = 0; i < 10; i++){
+            artistArray.push(response.topartists.artist[i]);
+                    
+            let regionArtist = document.createElement("li");
+            document.getElementById("regionList").appendChild(regionArtist);
+            regionArtist.textContent = artistArray[i].name;
+                    
+            let listeners = document.createElement("ul");
+            listeners.className = "subList";
+            regionArtist.appendChild(listeners);
+            let bullet = document.createElement("li");
+            bullet.className = "subBullet";
+            listeners.appendChild(bullet);
+            bullet.textContent = "Listeners: " + parseInt(artistArray[i].listeners).toLocaleString();
         }
-    })
+                
+                
+    })    
+        
+    event.preventDefault();  
+
+})
           
 }
